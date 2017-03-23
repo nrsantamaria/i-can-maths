d62f0d9d0696506de9bc00a43bcc74e96a66ea60
James Higgins and Nicole Santamaria <nicole@>
2017-03-23 14:33:55 -0700

    add content to index.html. add function to scripts to show specific triangle type based off user input.

---
+++ js/scripts.js
@@ -0,0 +1,21 @@
+$(document).ready(function() {
+  $("form#triangle").submit(function(event) {
+    var side1 = parseInt($("input#side1").val());
+    var side2 = parseInt($("input#side2").val());
+    var side3 = parseInt($("input#side3").val());
+
+    $(".answer").hide();
+
+      if (side1 === side2 && side2 === side3) {
+      $("#equal").show();
+    } else if (side1 + side2 <= side3 || side2 + side3 <= side1 || side3 + side1 <= side2){
+      $("#not").show();
+    } else if (side1 === side2 && side2 != side3 || side1 === side3 && side1 != side2 || side2 === side3 && side2 != side1) {
+      $("#iso").show();
+    } else if (side1 != side2 && side2 != side3 && side1 != side3){
+      $("#scal").show();
+    }
+  this.reset();
+    event.preventDefault();
+  });
+});
