4eb047a705d4912309ead560ea6b1eb140086067
Nicole Santamaria <nicolersantamaria@gmail.com>
2017-06-11 19:49:19 -0700

    create style sheets for each page and all pages. update cooking scripts.

--- js/cooking-scripts.js
+++ js/cooking-scripts.js
@@ -1,10 +1,9 @@
 $(document).ready(function() {
   $("form#cooking").submit(function(event) {
     event.preventDefault();
-    var convertOzToMl = function() {
-      var oz = parseFloat($("#ounces").val());
-      var result = oz * 29.5735;
-    };
-  $(".output").text(result);
+    var oz = parseFloat($("#ounces").val());
+    var result = (oz * 29.5735).toFixed(2);
+    $(".output").show();
+    $(".output p").text(result + " mL");
   });
 });
